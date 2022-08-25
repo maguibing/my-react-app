05b109b9b7afc006e4ed9307e45d572115763eff
maguibing <kuse012@163.com>
2022-08-25 17:40:08 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,30 @@ import './App.css';
 import React from 'react';
 import {useMouse} from './hooks/hooks'
 import {useRef, useEffect} from "react";
-
+// 使用useContext
+import Context from './hooks/useContext'
 
 const App = () => {
     // useState 的使用
     const mouse = useMouse()
     const target = useRef(null)
-    useEffect(()=>{
+    /**
+     * vue3 + vite + typesript + pinia + vueRouter4
+     */
+    useEffect(() => {
         target.current.focus()
-    },[])
+    }, [])
     return (
-        <React.Fragment>
-            <input type="text" ref={target}/>
-            <div className={'container'}>
-                <h3>{JSON.stringify(mouse)}</h3>
-            </div>
-        </React.Fragment>
+        <Context.Provider>
+            <React.Fragment>
+                <input type="text" ref={target}/>
+                <div className={'container'}>
+                    <h3>{JSON.stringify(mouse)}</h3>
+                    <h3>{JSON.stringify(mouse)}</h3>
+                </div>
+            </React.Fragment>
+        </Context.Provider>
+
     )
 }
 
---
+++ src/hooks/useContext.js
@@ -0,0 +1,6 @@
+import {createContext} from "react";
+
+export default createContext({
+    count: 0,
+    msg: "第一次使用createContext"
+})
